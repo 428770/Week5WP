4cb4127ddc3dd80c4eaab2d16cb401dbc0bdddf7
428770 <428770@edu.cwa.ac.uk>
2024-11-05 12:46:30 +0000

    5 nov

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from experience import views.py
+import experience.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('experience', experience.views.experience, name='experience'),
+    path('experience',experience.views.experience,name='experience'),
 ]
